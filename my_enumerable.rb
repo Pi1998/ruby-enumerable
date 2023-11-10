448f2a59543cf5a4f862746a676d800e6514fcf2
Shinn Thant Swam Ye <1shinnthantswamye@gmail.com>
2023-11-10 20:39:32 +0630

    Change file name

---
+++ my_enumerable.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+# This module provides custom enumerable methods.
+module MyEnumerable
+  def all?
+    arr = []
+    @list.each { |i| arr.push(i) if yield i }
+    puts arr.length == @list.length
+  end
+
+  def any?
+    @list.each do |i|
+      puts true if yield i
+    end
+    puts false
+  end
+
+  def filter
+    @array = []
+    @list.each { |i| @array.push(i) if yield i }
+    print "#{@array}\n"
+  end
+end
